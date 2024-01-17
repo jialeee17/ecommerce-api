affa6fc139fd4d45fb1408764b4a72a25e890322
JiaLe <jialeee0511@gmail.com>
2024-01-17 21:26:17 +0800

    update(migration): remove cascade update

--- database/migrations/2024_01_13_173245_create_states_table.php
+++ database/migrations/2024_01_13_173245_create_states_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('states', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('country_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('country_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
